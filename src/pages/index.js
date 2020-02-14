8b6e419990bc7572661c89d62ab0a6c4e18736f0
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-02-14 12:25:24 -0500

    Fix date object

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const SVG = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFFF99" fillOpacity="1" d="M0,288L48,272C96,256,192,224,288,218.7C384,213,480,235,576,213.3C672,192,768,128,864,90.7C960,53,1056,43,1152,37.3C1248,32,1344,32,1392,32L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 )
 
-const DEADLINE = new Date("April 4, 2020 12:00:00:00").getTime();
+const DEADLINE = new Date("April 4, 2020 12:00:00");
 class App extends Component {
   constructor(props) {
     super(props);
@@ -49,13 +49,12 @@ class App extends Component {
   }
 
   countdown() {
-    let today = new Date().getTime();
+    let today = new Date();
     let remaining = DEADLINE - today;
     let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
     let hours = Math.floor((remaining / (1000 * 60 * 60)) % 24);
     let minutes = Math.floor((remaining / 1000 / 60) % 60);
     let seconds = Math.floor((remaining / 1000) % 60);
-
     this.setState({ days, hours, minutes, seconds });
   }
 
