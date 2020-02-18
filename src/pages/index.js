716888780fb2472730d8cf40ef7cd19a2d26f280
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-02-17 21:44:15 -0500

    Fix timer edge cases

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ import Footer from '../components/footer'
 
 import "./styles.scss"
 
-const SVG = () => (
+const HeaderWaves = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFFF99" fillOpacity="1" d="M0,288L48,272C96,256,192,224,288,218.7C384,213,480,235,576,213.3C672,192,768,128,864,90.7C960,53,1056,43,1152,37.3C1248,32,1344,32,1392,32L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 )
 
-const DEADLINE = new Date("April 4, 2020 12:00:00");
+const DEADLINE = new Date("April 4, 2020 00:00:00").getTime();
 class App extends Component {
   constructor(props) {
     super(props);
@@ -26,12 +26,17 @@ class App extends Component {
   }
 
   static getDerivedStateFromProps() {
-    let today = new Date().getTime();
-    let remaining = DEADLINE - today;
-    let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((remaining / (1000 * 60 * 60)) % 24);
-    let minutes = Math.floor((remaining / 1000 / 60) % 60);
-    let seconds = Math.floor((remaining / 1000) % 60);
+    let now = new Date().getTime();
+    let t = DEADLINE - now;
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours =  Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+    if(t < 0) days = hours = minutes = seconds = 0;
     return {
       days: days,
       hours: hours,
@@ -41,7 +46,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.countdown(), 1000);
+    this.timerID = setInterval(this.countdown, 1000);
   }
 
   componentWillUnmount() {
@@ -49,13 +54,19 @@ class App extends Component {
   }
 
   countdown() {
-    let today = new Date();
-    let remaining = DEADLINE - today;
-    let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((remaining / (1000 * 60 * 60)) % 24);
-    let minutes = Math.floor((remaining / 1000 / 60) % 60);
-    let seconds = Math.floor((remaining / 1000) % 60);
-    this.setState({ days, hours, minutes, seconds });
+    let now = new Date().getTime();
+    let t = DEADLINE - now;
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours =  Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+    
+    if(t > 0) this.setState({ days, hours, minutes, seconds });
+    else clearInterval(this.timerID);
   }
 
   render() {
@@ -85,7 +96,7 @@ class App extends Component {
             </div>
           </div>
         </section>
-        <SVG />
+        <HeaderWaves />
         <About />
         <Itenerary />
         <FAQ />
