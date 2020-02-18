a362463988568cf6407fc4eae5bfba9fcda79f7b
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-02-17 22:06:16 -0500

    Fix timer syntax

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,10 +33,9 @@ class App extends Component {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / 1000);
 
-    if (hours < "10") { hours = "0" + hours; }
-    if (minutes < "10") { minutes = "0" + minutes; }
-    if (seconds < "10") { seconds = "0" + seconds; }
-    if(t < 0) days = hours = minutes = seconds = 0;
+    if (hours < "10" && hours > "0") { hours = "0" + hours; }
+    if (minutes < "10" && hours > "0") { minutes = "0" + minutes; }
+    if (seconds < "10" && hours > "0") { seconds = "0" + seconds; }
     return {
       days: days,
       hours: hours,
@@ -61,9 +60,9 @@ class App extends Component {
     let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((t % (1000 * 60)) / 1000);
 
-    if (hours < "10") { hours = "0" + hours; }
-    if (minutes < "10") { minutes = "0" + minutes; }
-    if (seconds < "10") { seconds = "0" + seconds; }
+    if (hours < "10" && hours > "0") { hours = "0" + hours; }
+    if (minutes < "10" && hours > "0") { minutes = "0" + minutes; }
+    if (seconds < "10" && hours > "0") { seconds = "0" + seconds; }
 
     if(t > 0) this.setState({ days, hours, minutes, seconds });
     else clearInterval(this.timerID);
