16e7f74fe60c39b7fc7277af41105354a3ed9b21
Edwin Carbajal <exaviercarbajal@gmail.com>
2020-02-22 21:07:22 -0500

    Update team bios

--- src/components/team.js
+++ src/components/team.js
@@ -15,7 +15,7 @@ export default () => (
     <section className="hero is-medium has-text-centered">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title" style={{ paddingBottom: '50px' }}>Our Team</h1>
+          <h1 className="title" style={{ paddingBottom: '50px' }}>Meet The Team</h1>
           <div className="columns is-12">
             <div className="column">
               <figure className="image container is-128x128">
@@ -23,6 +23,8 @@ export default () => (
               </figure>
               <p className="has-text-weight-bold">Dr. Mohammad Azhar</p>
               <p>Advisor</p>
+              <p>Assistant Professor of CIS Dept.</p>
+              <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
             </div>
             <div className="column">
               <figure className="image container is-128x128">
@@ -30,6 +32,8 @@ export default () => (
               </figure>
               <p className="has-text-weight-bold">Katherine Bedoya</p>
               <p>Director of Logistics</p>
+              <p>Employer Relations Coordinator</p>
+              <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
             </div>
             <div className="column">
               <figure className="image container is-128x128">
@@ -45,7 +49,8 @@ export default () => (
                 <img alt="" className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
               </figure>
               <p className="has-text-weight-bold">Michael Hicks</p>
-              <p>Pending</p>
+              <p>Director of Hacker Experience</p>
+              <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
             </div>
           </div>
         <div className="columns is-12">
@@ -64,13 +69,15 @@ export default () => (
             </figure>
             <p className="has-text-weight-bold">Fernanda Carvalho Santos</p>
             <p>Director of Marketing + Design</p>
+            <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
           </div>
           <div className="column">
             <figure className="image container is-128x128">
               <img alt="" className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
             </figure>
             <p className="has-text-weight-bold">Thierry Thesatus</p>
-            <p>Pending</p>
+            <p>Associate Director of Career Services</p>
+            <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
           </div>
           <div className="column">
             <figure className="image container is-128x128">
@@ -78,6 +85,8 @@ export default () => (
             </figure>
             <p className="has-text-weight-bold">Elizabeth Yan</p>
             <p>Director of IT + Swag</p>
+            <p>Information Technology Coordinator</p>
+            <p><a style={{ color: 'black', textDecoration: 'underline' }}href="/" target="_blank" rel="noopener noreferrer">Link</a></p>
           </div>
         </div>
         </div>
